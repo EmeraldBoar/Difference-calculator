06882b2f95c76403862d7318e4010a323ca59296
EmeraldBoar <kozlovkv174@gmail.com>
2021-01-31 15:42:39 +0300

    Adds interface

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import { Command } from 'commander';
+import path from 'path';
+import fs from 'fs';
+
+const getDiff = (filepath1, filepath2) => {
+  const file1 = JSON.parse(fs.readFileSync(path.resolve(filepath1), 'utf8'));
+  const file2 = JSON.parse(fs.readFileSync(path.resolve(filepath2), 'utf8'));
+};
 
 const getInterface = () => {
   const program = new Command();
 
   program
+    .version('0.0.1', '-V, --version', 'output the version number')
     .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1', '-V, --version', 'output the version number');
+    .arguments('<filepath1>, <filepath2>')
+    .option('--format [type]', 'output format')
+    .action(getDiff);
 
   program.parse(process.argv);
 };
